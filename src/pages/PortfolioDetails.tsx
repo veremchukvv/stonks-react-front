b4e2e3d60288c0d334f55d7ec35009ba036c40ff
veremchukvv@gmail.com <veremchukvv@gmail.com>
2021-11-29 23:52:40 +0300

    preparations for aggregated stock view

--- src/pages/PortfolioDetails.tsx
+++ src/pages/PortfolioDetails.tsx
@@ -4,6 +4,7 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 // import FundList from '../components/FundList';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import { AuthContext } from '../context/authContext';
+import * as _ from "lodash";
 
 const PortfolioDetails = () => {
     const auth = useContext(AuthContext)
@@ -13,13 +14,14 @@ const PortfolioDetails = () => {
     const deletePortfolio = async () => {
         await fetch(`http://localhost:8000/api/v1/portfolio/${portfolioId}`, {
             method: 'DELETE',
-            headers: {'Content-Type':'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
-            })
-            history.push('/')
-        }
+        })
+        history.push('/')
+    }
 
-    const [stocks, setStocks] = useState<any[]>([])
+    const [deals, setDeals] = useState<any[]>([])
+    // const [stocks, setStocks] = useState<any[]>([])
     // const [portftolio, setPortfolio] = useState<any[]>([])
     const portfolioId = useParams<any>().id
     auth.portfolioID = portfolioId
@@ -30,8 +32,9 @@ const PortfolioDetails = () => {
         })
         const content = await response.json()
 
-        setStocks(content["StocksResp"])
+        setDeals(content["StocksResp"])
         // setPortfolio(content["PortfolioResp"])
+
     }, [portfolioId]
     )
 
@@ -40,8 +43,28 @@ const PortfolioDetails = () => {
             fetchStocks()
         }, [fetchStocks]
     )
+
+    if (deals != null) {
+
+        let grouped = _.groupBy(deals, 'ticker')
+        let tickers = _.uniqBy(deals, 'ticker')
+
+        let stocksInfo = tickers.map(({ value, amount, ...item }) => item);
+
+        let values = _.map(grouped, (objs:any, key:any) => ({
+            'ticker': key,
+            'value': _.sumBy(objs, 'value') }))
+
+        let amounts = _.map(grouped, (objs:any, key: any) => ({
+            'ticker': key,
+            'amount' : _.sumBy(objs, 'amount')
+        }))
+
+        let res = _.merge(stocksInfo, values, amounts)
+    }
+
     // if (!stocks.length || stocks === null) {
-        if (stocks === null || !stocks.length ) {
+    if (deals === null || !deals.length) {
         return (
             <div>
                 You don't have stocks yet.
@@ -53,7 +76,7 @@ const PortfolioDetails = () => {
     else {
         return (
             <div>
-                <table>
+                {/* <table>
                     <thead>
                         <tr>
                             <th>Id</th>
@@ -82,6 +105,36 @@ const PortfolioDetails = () => {
                             )
                         })}
                     </tbody>
+                </table> */}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Ticker</th>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Cost</th>
+                            <th>Amount</th>
+                            <th>Total Cost</th>
+                            <th>Currency</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {deals.map((deal, index) => {
+                            return (
+                                <tr key={deal.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{deal.ticker}</td>
+                                    <td>{deal.name}</td>
+                                    <td>{deal.type}</td>
+                                    <td>{deal.cost}</td>
+                                    <td>{deal.amount}</td>
+                                    <td>{deal.value}</td>
+                                    <td>{deal.currency}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </table>
                 {/* <h1>Stocks</h1>
                     <StockList />
@@ -89,7 +142,7 @@ const PortfolioDetails = () => {
                     <BondList />
                     <h1>Funds</h1>
                     <FundList /> */}
-                <div style={{display: 'flex', justifyContent: 'center', padding: '0 2rem'}}>
+                <div style={{ display: 'flex', justifyContent: 'center', padding: '0 2rem' }}>
                     <Link to="/" className="btn btn-primary" style={{ marginRight: 10 }}>Back</Link>
                     <Link to="/stockmarket" className="btn btn-primary" style={{ marginRight: 10 }}>Market</Link>
                     <Link to="/" className="btn btn-danger" style={{ marginRight: 10 }} onClick={deletePortfolio}>Delete</Link>
